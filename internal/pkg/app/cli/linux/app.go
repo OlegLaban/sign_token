abb61f6596ae5fe2293d333cd8d19799f7ec8dd7
Laban Oleg <oleg19432@gmail.com>
2026-02-19 07:40:35 +0300

    add-encrypt-logic: add encrypt logic

--- internal/pkg/app/cli/linux/app.go
+++ internal/pkg/app/cli/linux/app.go
@@ -2,7 +2,10 @@ package linux
 
 import (
 	"log"
-	"time"
+
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 )
 
 type linuxAPP struct{}
@@ -12,6 +15,13 @@ func NewAPP() *linuxAPP {
 }
 
 func (la *linuxAPP) Run() {
-	now := time.Now().Unix()
-	log.Println(now)
+	a := app.New()
+	w := a.NewWindow("Sign")
+	input := widget.NewEntry()
+
+	w.SetContent(container.NewVBox(widget.NewLabel("some text"), input, widget.NewButton("Send", func() {
+		log.Println("Content was:", input.Text)
+		input.SetText("")
+	})))
+	w.ShowAndRun()
 }
--- internal/pkg/key_generator/generator.go
+++ internal/pkg/key_generator/generator.go
@@ -1,15 +1,67 @@
 package keygenerator
 
 import (
+	"encoding/json"
+	"errors"
+	"strconv"
+
 	"github.com/OlegLaban/sing_token/internal/domain"
 )
 
-type generator struct{}
+type Logger interface {
+	Infof(string, ...any)
+	Debugf(string, ...any)
+	Error(string, error)
+}
 
-func NewGenerator() *generator {
-	return &generator{}
+type Crypter interface {
+	Encrypt(string) (string, error)
+	Decrypt(string) (string, error)
+}
+
+type generator struct {
+	c Crypter
+	l Logger
+}
+
+type key struct {
+	Indentify string `json:"id"`
+	DateTime  string `json:"date_time"`
+}
+
+func NewGenerator(c Crypter, l Logger) *generator {
+	return &generator{c: c, l: l}
 }
 
 func (g *generator) Generate(p domain.Payload) (string, error) {
-	return "", nil
+	tokenData, err := g.convertPayload(p)
+	if err != nil {
+		g.l.Error("can`t encode payload to string %s", err)
+		return "", errors.Join(ErrCantConvertKey, err)
+	}
+	g.l.Debugf("payload was converted to tokenData - %s", tokenData)
+	encryptedText, err := g.c.Encrypt(tokenData)
+	if err != nil {
+		g.l.Error("can`t encrypt payload", err)
+		return "", errors.Join(ErrCantEncryptKey, err)
+	}
+	g.l.Debugf("token data was encrypted - %s", encryptedText)
+
+	return encryptedText, nil
+}
+
+func (g *generator) convertPayload(p domain.Payload) (string, error) {
+	keyStruct := key{
+		Indentify: p.FullName,
+		DateTime:  strconv.Itoa(p.Date),
+	}
+
+	data, err := json.Marshal(keyStruct)
+	if err != nil {
+		g.l.Error("can`t marshal key struct to json", err)
+		return "", err
+	}
+	g.l.Debugf("payload was converted to json - %d", len(data))
+
+	return string(data), nil
 }
